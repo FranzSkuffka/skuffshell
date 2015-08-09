ecd4d8c71999a46827881f0d53ea96a0010ef3bb
Jan Wirth <jottenwe@gmail.com>
2015-08-09 11:54:51 +0200

    add v, add comments, add man pages

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,13 +12,18 @@ elif [ $system == "Linux" ]; then
 else
 	echo "System not supported!"
 fi
+
+# install zshell
 chsh -s /bin/zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# install vim config
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 
 # install shell plugins
 mkdir ~/.shellplugins
 curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.shellplugins/z.sh
+curl https://raw.githubusercontent.com/rupa/z/master/z.1 > /usr/local/share/man/man1/z.1
 
 # download cheat sheets
 mkdir ~/.cheatsheets
@@ -27,3 +32,12 @@ curl https://gist.githubusercontent.com/MohamedAlaa/2961058/raw/b209816bf94f9de5
 # customize .zshrc
 echo ". ~/.shellplugins/z.sh" >> ~/.zshrc
 echo "set -o 'vi'" >> ~/.zshrc
+
+# install v
+curl https://raw.githubusercontent.com/rupa/v/master/v > usr/local/bin/v
+sudo chmod 755 usr/local/bin/v
+curl https://raw.githubusercontent.com/rupa/v/master/v.1 > /usr/local/share/man/man1/v.1
+
+
+# update man pages
+sudo mandb
